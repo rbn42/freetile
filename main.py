6963613809f3ab0490c3ee22f5f2894fd240f9fa
rbn42 <bl100@students.waikato.ac.nz>
2016-08-23 11:32:35 +1200

    regularize

--- main.py
+++ main.py
@@ -353,8 +353,8 @@ if __name__ == '__main__':
                 # K-D tree layout, combine this command with dmenu or rofi.
                 logging.info('regularize layout')
                 regularize()
-                regularize()
-                regularize()
+                # regularize()
+                # regularize()
 
         elif arguments['grow']:
             if arguments['width']:
