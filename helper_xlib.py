0130dca0a3da94f6fc921f0e82ddbbe4a1e888c2
rbn42 <bl100@students.waikato.ac.nz>
2017-07-28 00:17:30 +1200

    to xlib

--- helper_xlib.py
+++ helper_xlib.py
@@ -1,10 +1,33 @@
 from Xlib import X, display, Xutil, protocol
-disp = display.Display()
-#winid = window.winId()
+from util_wmctrl import unmaximize_one
+from util_xprop import get_window_frame_size
 
-def arrange(layout, windows):
-    for winid,lay in zip(windows,layout):
-        x,y,width,height=lay
-        window_xlib = disp.create_resource_object('window', winid)
-        window_xlib.configure(x=x, y=y, width=width, height=height)
-    disp.flush()
+disp = display.Display()
+
+
+def arrange(layout, windows, sync=True):
+    for winid, lay in zip(windows, layout):
+        x, y, width, height = lay
+        unmaximize_one(winid, sync=False)
+        move_window(winid, *lay, sync=False)
+    if sync:
+        disp.flush()
+
+
+def move_window(windowid, x, y, w, h, sync=True):
+    window_xlib = disp.create_resource_object('window', windowid)
+
+    f_left, f_right, f_top, f_bottom = get_window_frame_size(windowid)
+    w -= f_left + f_right
+    h -= f_top + f_bottom
+
+    wm_normal_hints = window_xlib.get_wm_normal_hints()
+    # TODO
+    print(wm_normal_hints)
+    if 'Stati' in str(wm_normal_hints):
+        y += f_top
+        x += f_left
+
+    window_xlib.configure(x=x, y=y, width=w, height=h)
+    if sync:
+        disp.flush()
