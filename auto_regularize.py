36f3cd077a357cd9e382f3aeb6a5ab56b9d4a1c9
rbn42 <bl100@students.waikato.ac.nz>
2017-07-31 20:42:52 +1200

    auto_regularize.py

--- auto_regularize.py
+++ auto_regularize.py
@@ -1,12 +1,34 @@
 """
 TODO
 """
+import Xlib
 from Xlib import X, display, Xutil, protocol
 import os
+import ewmh
+ewmh = ewmh.EWMH()
 disp = display.Display()
 root = disp.screen().root
 root.change_attributes(event_mask=X.SubstructureNotifyMask)
 
+IGNORE_TYPES = [
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_DESKTOP'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_DOCK'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_TOOLBAR'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_MENU'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_UTILITY'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_SPLASH'),
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_DIALOG'),
+    390,  # emerald
+    391,  # volnoti
+    348,  # docky setting
+    #        disp.intern_atom('_NET_WM_WINDOW_TYPE_NORMAL'),
+]
+ALLOW_TYPES = [
+    disp.intern_atom('_NET_WM_WINDOW_TYPE_NORMAL'),
+]
+print(IGNORE_TYPES)
+IGNORE_TYPES = set(IGNORE_TYPES)
+ALLOW_TYPES= set(ALLOW_TYPES)
 while True:
     e = disp.next_event()
     if e.type in (
@@ -14,8 +36,26 @@ while True:
             X.MapNotify,
             # X.ReparentNotify,
             # X.DestroyNotify
-            ):
-        os.system('python ./main.py layout regularize &> ~/dev/null')
-        print(e)
-        for _ in range(disp.pending_events()):
-            disp.next_event()
+    ):
+        win = e.window
+        try:
+            c = win.get_wm_class()
+            n = win.get_wm_name()
+            t = ewmh.getWmWindowType(win)
+            print([e.type, n, c, t, ])
+            if len(ALLOW_TYPES.intersection(t)) < 1:
+                print('continue%s' % str(t))
+                continue
+            if not None == c and 'Popup' in c:
+                print('continue%s' % str(c))
+                continue
+            if 'rofi' == n:
+                print('continue%s' % n)
+                continue
+        except Xlib.error.BadWindow:
+            print("error%s" % e.type)
+            continue
+        print('excute')
+        os.system('python ./main.py layout regularize &> /dev/null')
+#        for _ in range(disp.pending_events()):
+#            disp.next_event()
