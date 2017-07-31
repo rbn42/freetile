9856f3f3d00d3912fac081fcc7e6263fd2cafb14
rbn42 <bl100@students.waikato.ac.nz>
2017-07-31 19:48:47 +1200

    auto_regularize.py

--- auto_regularize.py
+++ auto_regularize.py
@@ -4,12 +4,28 @@ TODO
 from Xlib import X, display, Xutil, protocol
 import time
 import os
+import ewmh
+ewmh = ewmh.EWMH()
 disp = display.Display()
-t=time.time()
+root = disp.screen().root
+root.change_attributes(event_mask=X.SubstructureNotifyMask)
+
+#t = time.time()
 while True:
-    if time.time()-t>0.1:
-        t=time.time()
-        e = disp.next_event()
-#        if e.type in (X.MappingNotify,X.UnmapNotify):
-        os.system('python ./main.py layout regularize')
+    #    if time.time()-t>0.1:
+    #    t = time.time()
+    e = disp.next_event()
+    if e.type in (
+            X.UnmapNotify,
+            X.MapNotify,
+            # X.ReparentNotify,
+            # X.DestroyNotify
+    ):
+        os.system('python ./main.py layout regularize &> ~/dev/null')
+        print(e)
+        for _ in range(disp.pending_events()):
+            disp.next_event()
+
 #            print(e.type)
+        # print(e.window.id)
+#                print(ewmh.getWmName(e.window))
