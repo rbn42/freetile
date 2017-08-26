431fcfb684a10820bed2e6d314460f14f0ad1be9
rbn42 <bl100@students.waikato.ac.nz>
2017-08-27 01:53:50 +1200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='freetile',
-    version='0.1.0',
+    version='0.1.1',
     description='freely tiling script for X',
     url='http://github.com/rbn42/freetile',
-    download_url='https://github.com/rbn42/freetile/archive/0.1.0.tar.gz',
+    download_url='https://github.com/rbn42/freetile/archive/0.1.1.tar.gz',
     author='rbn42',
     author_email='bl100@students.waikato.ac.nz',
     license='MIT',
