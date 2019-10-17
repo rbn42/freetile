837fc6a5e47f2705a80b74d1f79e797188e9b3c7
rbn42 <bl100@students.waikato.ac.nz>
2019-10-17 12:54:59 +0800

    Fix issue

--- freetile/monitor.py
+++ freetile/monitor.py
@@ -33,13 +33,16 @@ class Monitor:
                     self.x, self.y, self.width, self.height = item
                     if xywh0 is None:
                         return (0, 0, self.width, self.height)
-                    x0,y0,w0,h0=xywh0
+                    x0, y0, w0, h0 = xywh0
 
                     xin = win.x < x0 + self.x + self.width and win.x + win.width > x0 + self.x
                     yin = win.y < y0 + self.y + self.width and win.y + win.width > y0 + self.y
                     if xin and yin:
                         self.available = True and len(monitors) > 2
-                        return self.x + x0, self.y + y0, \
+                        if (self.x < 1 and self.y < 1):
+                            return self.x + x0, self.y + y0, w0, h0
+                        else:
+                            return self.x + x0, self.y + y0, \
                             min(self.width, w0 + x0 - self.x), \
                             min(self.height, h0 + y0 - self.y)
         self.available = False
